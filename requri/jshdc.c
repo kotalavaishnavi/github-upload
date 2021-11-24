34a9d04bda3781e4f83a78576bc2d0fa4abf4aa0
kotalavaishnavi <94115776+kotalavaishnavi@users.noreply.github.com>
2021-11-24 16:53:49 +0530

    Create jshdc.c

---
+++ requri/jshdc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define IN 1
+#define OUT 0
+
+void Addbook();
+void Searchbook();
+void Displaybook();
+void Author();
+void Titlelist();
+void Stock();
+void Issue();
+void bookret();
+void Addmembr();
+void Exit();
+
+char info[500];
